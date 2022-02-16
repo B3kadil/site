174e351c6220841a38e623fa93d855039ad76239
B3kadil <bekadilseksenbaev03@gmail.com>
2022-02-17 00:22:07 +0600

    the file was changed

--- scrypt/scrypt.js
+++ scrypt/scrypt.js
@@ -2,4 +2,3 @@ darkMode.onclick() {
     document.body.classList.toggle("dark")
     darkMode.innerText = document.body.classList.contains("dark") ? "" : ""
 }
-console.log("Sheshen amy nahu metirit na")

84c7527429dd426d284ea3eda2907a15b903f3f2
B3kadil <bekadilseksenbaev03@gmail.com>
2022-02-19 23:34:08 +0600

    to added dark mode button

--- scrypt/scrypt2.js
+++ scrypt/scrypt2.js
@@ -1,5 +1,4 @@
 let dark = localStorage.getItem("darkMode")
-const darkMode = document.querySelector(".darkMode")
 const enableDarkmode = () => {
     document.body.classList.add('dark')
     localStorage.setItem('dark', 'enabled')
@@ -8,28 +7,31 @@ const disenableDarkmode = () => {
     document.body.classList.remove('dark')
     localStorage.setItem('dark', null)
 }
-darkMode.addEventListener("click", function() {
+const darkMode2 = document.querySelector(".darkMode2")
+darkMode2.addEventListener("click", function() {
     dark = localStorage.getItem('dark');
     if (dark !== 'enabled') {
         enableDarkmode();
-        document.querySelector('.rectangle-2').style.background = '#3131F1';
-        document.querySelector('.rectangle-2').style.borderColor = '#040404';
+        document.querySelector('.rectangle').style.background = '#3131F1';
+        document.querySelector('.rectangle').style.borderColor = '#040404';
         document.querySelector('.line-1').style.background = '#3131F1';
         document.querySelector('.head-1').style.color = '#DCD3BB';
         document.querySelector('.logo').style.color = '#DCD3BB';
         document.querySelector('.text-1').style.color = '#DCD3BB';
         document.querySelector('.text-2').style.color = '#DCD3BB';
-        document.querySelector('.reg').style.color = '#3131F1';
+        document.querySelector('.log').style.color = '#3131F1';
         document.querySelector('.head-2').style.color = '#DCD3BB';
         document.querySelector('.text-3').style.background = '#00141f';
         document.querySelector('.head-3').style.color = '#DCD3BB';
         document.querySelector('.text-4').style.background = '#00141f';
+        document.querySelector('.head-4').style.color = '#DCD3BB';
+        document.querySelector('.text-5').style.background = '#00141f';
+        document.querySelector('.head-5').style.color = '#DCD3BB';
+        document.querySelector('.text-6').style.background = '#00141f';
         document.querySelector('.main').style.background = '#00141f';
-        document.querySelector('.text-5').style.color = '#DCD3BB';
-        document.querySelector('.rectangle-1').style.background = 'black';
         let img = document.querySelector('.background');
         img.setAttribute('src', 'img/wall-negative.jpg');
-        document.querySelector('.darkMode').style.background = 'black';
+        document.querySelector('.darkMode2').style.background = 'black';
         document.querySelector('.darkOn').innerHTML = ('Dark')
         document.querySelector('.darkOn').style.color = '#DCD3BB';
         document.querySelector('.light').setAttribute('src', 'img/moonmode.png');
@@ -40,24 +42,26 @@ darkMode.addEventListener("click", function() {
         console.log('dark on')
     } else {
         disenableDarkmode();
-        document.querySelector('.rectangle-2').style.background = '#FF432A';
-        document.querySelector('.rectangle-2').style.borderColor = '#FF432A';
+        document.querySelector('.rectangle').style.background = '#FF432A';
+        document.querySelector('.rectangle').style.borderColor = '#FF432A';
         document.querySelector('.line-1').style.background = '#FF432A';
         document.querySelector('.head-1').style.color = 'black';
         document.querySelector('.logo').style.color = 'black';
         document.querySelector('.text-1').style.color = 'black';
         document.querySelector('.text-2').style.color = 'black';
-        document.querySelector('.reg').style.color = '#FF432A';
+        document.querySelector('.log').style.color = '#3131F1';
         document.querySelector('.head-2').style.color = 'black';
         document.querySelector('.text-3').style.background = 'white';
         document.querySelector('.head-3').style.color = 'black';
         document.querySelector('.text-4').style.background = 'white';
+        document.querySelector('.head-4').style.color = 'black';
+        document.querySelector('.text-5').style.background = 'white';
+        document.querySelector('.head-5').style.color = 'black';
+        document.querySelector('.text-6').style.background = 'white';
         document.querySelector('.main').style.background = 'white';
-        document.querySelector('.text-5').style.color = 'black';
-        document.querySelector('.rectangle-1').style.background = 'black';
         let img = document.querySelector('.background');
         img.setAttribute('src', 'img/wall.png');
-        document.querySelector('.darkMode').style.background = '#f8f8f8';
+        document.querySelector('.darkMode2').style.background = '#f8f8f8';
         document.querySelector('.darkOn').innerHTML = ('Light')
         document.querySelector('.darkOn').style.color = 'black';
         document.querySelector('.light').setAttribute('src', 'img/sunmode.png');
